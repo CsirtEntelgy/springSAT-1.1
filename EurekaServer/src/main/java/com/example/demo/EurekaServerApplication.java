9912e8eb7966feb608598932fe49d6cad1d6a5ac
Luis Martinez <luis.martinez.ipn@gmail.com>
2019-11-21 05:57:20 -0600

    eureka server

--- EurekaServer/src/main/java/com/example/demo/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/example/demo/EurekaServerApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class AppProductosApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppProductosApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
